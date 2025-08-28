188ed2a3976689938407eaeab1470791e651f75a
Filipe <fstraubel10@gmail.com>
2025-08-28 17:25:31 -0300

    Feito Testes Unitarios no Modulo de Filmes

---
+++ ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeAppServiceTests.cs
@@ -0,0 +1,113 @@
+﻿using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloFilme
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade de Filmes")]
+    public sealed class FilmeAppServiceTests
+    {
+        private Mock<IRepositorioFilme>? repositorioFilmeMock;
+        private Mock<ITenantProvider>? tenantProviderMock;
+        private Mock<IUnitOfWork>? unitOfWorkMock;
+        private Mock<ILogger<FilmeAppService>>? loggerMock;
+
+        private FilmeAppService? filmeAppService;
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            repositorioFilmeMock = new Mock<IRepositorioFilme>();
+            tenantProviderMock = new Mock<ITenantProvider>();
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            loggerMock = new Mock<ILogger<FilmeAppService>>();
+
+            filmeAppService = new FilmeAppService(
+                tenantProviderMock.Object,
+                repositorioFilmeMock.Object,
+                unitOfWorkMock.Object,
+                loggerMock.Object
+            );
+        }
+
+        [TestMethod]
+        public void Cadastrar_DeveRetornarOk_QuandoFilmeForValido()
+        {
+            // Arrange
+            var genero = new GeneroFilme("Comedia");
+
+            var filme = new Filme("Debi & Loide", 90, false, genero);
+
+            var filmeTeste = new Filme("Teste", 120, false, genero);
+
+            repositorioFilmeMock?
+                .Setup(r => r.SelecionarRegistros())
+                .Returns(new List<Filme>() { filmeTeste });
+            // Act
+            var resultado = filmeAppService?.Cadastrar(filme);
+
+            // Assert
+
+            repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Once);
+
+            unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+            Assert.IsNotNull(resultado);
+            Assert.IsTrue(resultado.IsSuccess);
+
+        }
+
+        [TestMethod]
+        public void Cadastrar_DeveRetornarFalha_QuandoCampoObrigatorioForVazio()
+        {
+            // Arrange
+            var genero = new GeneroFilme("Comedia");
+            var filme = new Filme("", 90, false, genero);
+
+            repositorioFilmeMock?.Setup(r => r.SelecionarRegistros())
+                .Returns(new List<Filme>() { filme });
+
+            // Act
+            var resultado = filmeAppService?.Cadastrar(filme);
+
+            // Assert
+            repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Never);
+
+            unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+
+            Assert.IsNotNull(resultado);
+            Assert.IsTrue(resultado.IsFailed);
+        }
+
+        [TestMethod]
+        public void Cadastrar_DeveRetornarFalha_QuandoFilmeJaExistir()
+        {
+            // Arrange
+            var genero = new GeneroFilme("Comedia");
+
+            var filme = new Filme("Debi & Loide", 90, false, genero);
+
+            var filmeTeste = new Filme("Debi & Loide", 90, false, genero);
+
+            repositorioFilmeMock?
+                .Setup(r => r.SelecionarRegistros())
+                .Returns(new List<Filme>() { filmeTeste });
+            
+            // Act
+            var resultado = filmeAppService?.Cadastrar(filme);
+
+            // Assert
+            repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Never);
+
+            unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+
+            Assert.IsNotNull(resultado);
+            Assert.IsTrue(resultado.IsFailed);
+        }
+    }
+}
---
+++ ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeTests.cs
@@ -0,0 +1,31 @@
+﻿using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloFilme
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade de Filmes")]
+    public sealed class FilmeTests
+    {
+        private Filme? filme;
+
+        [TestMethod]
+        public void Deve_Adicionar_Filme_Corretamente() 
+        {
+            // Arrange
+            var genero = new GeneroFilme("Comedia");
+
+            filme = new Filme("Debi & Loide", 50, false, genero);
+
+            //Act
+            var filmeAdicionado = filme;
+
+            //Assert
+
+            var contemFilme = filme.Titulo.Contains("Debi & Loide");
+
+            Assert.IsTrue(contemFilme);
+        }
+
+    }
+}
