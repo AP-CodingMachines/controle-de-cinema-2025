ebd33149601d09e9cfaa5f0e06d630e0df4f11b5
Filipe <fstraubel10@gmail.com>
2025-08-31 16:46:31 -0300

    Feito testes unitarios no modulo Sala

---
+++ ControleDeCinema.Testes.Unidade/ModuloSala/SalaAppServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloSala;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloSala;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloSala
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade de Salas")]
+    public sealed class SalaAppServiceTests
+    {
+        private Mock<IRepositorioSala>? repositorioSalaMock;
+        private Mock<ITenantProvider>? tenantProviderMock;
+        private Mock<IUnitOfWork>? unitOfWorkMock;
+        private Mock<ILogger<SalaAppService>>? loogerMock;
+
+        private SalaAppService? salaAppService;
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            repositorioSalaMock = new Mock<IRepositorioSala>();
+            tenantProviderMock = new Mock<ITenantProvider>();
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            loogerMock = new Mock<ILogger<SalaAppService>>();
+
+            salaAppService = new SalaAppService(
+                tenantProviderMock.Object,
+                repositorioSalaMock.Object,
+                unitOfWorkMock.Object,
+                loogerMock.Object
+            );
+        }
+
+        [TestMethod]
+        public void Cadastrar_DeveRetornarOk_QuandoSalaForValida()
+        {
+            // Arrange
+            var sala = new Sala(1, 100);
+
+            var salaTeste = new Sala(2, 150);
+            
+            repositorioSalaMock?
+                .Setup(r => r.SelecionarRegistros())
+                .Returns(new List<Sala>() { salaTeste });
+            
+            // Act
+            var resultado = salaAppService?.Cadastrar(sala);
+
+            // Assert
+            repositorioSalaMock?.Verify(r => r.Cadastrar(sala), Times.Once);
+
+            unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+            Assert.IsNotNull(resultado);
+            Assert.IsTrue(resultado?.IsSuccess);
+        }
+
+        [TestMethod]
+        public void Cadastrar_DeveRetornarErro_QuandoSalaForDuplicada()
+        {
+            // Arrange
+            var sala = new Sala(1, 100);
+
+            var salaTeste = new Sala(1, 150);
+            
+            repositorioSalaMock?
+                .Setup(r => r.SelecionarRegistros())
+                .Returns(new List<Sala>() { salaTeste });
+            
+            // Act
+            var resultado = salaAppService?.Cadastrar(sala);
+
+            // Assert
+            repositorioSalaMock?.Verify(r => r.Cadastrar(sala), Times.Never);
+
+            unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+
+            Assert.IsNotNull(resultado);
+            Assert.IsTrue(resultado?.IsFailed);
+            
+        }
+    }
+}
---
+++ ControleDeCinema.Testes.Unidade/ModuloSala/SalaTests.cs
@@ -0,0 +1,25 @@
+﻿using ControleDeCinema.Dominio.ModuloSala;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloSala
+{
+    [TestClass]
+    [TestCategory("Teste de Unidade de Salas")]
+    public sealed class SalaTests
+    {
+        private Sala? sala;
+
+        [TestMethod]
+        public void Deve_Adicionar_Sala_Corretamente()
+        {
+            // Arrange
+            sala = new Sala(1, 100);
+
+            //Act
+            var salaAdicionada = sala;
+
+            //Assert
+            Assert.AreEqual(1, sala.Numero);
+            
+        }
+    }
+}
